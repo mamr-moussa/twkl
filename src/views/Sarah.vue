98d72fc1fd9eb01d0d9284d439023257627365e1
Amr <muhammad.amro.moussa@gmail.com>
2022-01-23 06:58:15 +0200

    fix: fix sarah

--- src/views/Sarah.vue
+++ src/views/Sarah.vue
@@ -1,50 +1,51 @@
 <template>
   <div class="home">
-    <div class="overlay" @click="refreshDate"/>
+    <div class="overlay" @click="refreshDate" />
     <svg viewBox="0 0 200 200" class="sarah-svg">
-  <path d="   M12 2L188 2 
+      <path
+        d="   M12 2L188 2 
               Q 198,2 198,20 L 198 188 
               Q198,198 188,198 L12 198 
               Q2,198 2,188 L2 12 
               Q2,2 2,12z
-              " stroke="white" stroke-width="10" fill="none"/>
-</svg>
-<div class="sarah-font" v-if="curDate" style="">
-  {{ fromArabicToASCII(curDate) }}
-</div>
+              "
+        stroke="white"
+        stroke-width="10"
+        fill="none"
+      />
+    </svg>
+    <div class="sarah-font" v-if="curDate" style="">
+      {{ fromArabicToASCII(curDate) }}
+    </div>
   </div>
 </template>
 
 <script>
-import moment from 'moment'
+import moment from "moment";
 export default {
   name: "Sarah",
-  components: {
-  },
-  data(){
-    return{
-      curDate: null
+  components: {},
+  data() {
+    return {
+      curDate: null,
     };
   },
-  mounted(){
-    moment.locale('ar')
-    this.curDate = moment().format('dddd D MMMM , HH:mm A')
-    console.log()
+  mounted() {
+    moment.locale("ar");
+    this.curDate = moment().format("dddd D MMMM , HH:mm A");
+    console.log();
   },
-  methods:{
-          fromArabicToASCII(arabic) {
-         return arabic.replace(
-          /[\u0660-\u0669\u06F0-\u06F9]/g,
-          function(a) {
-            return String.fromCharCode((a.charCodeAt(0)&15)+48);
-            }
-          );
-        },
-        refreshDate(){
-           moment.locale('ar')
-           this.curDate = moment().format('dddd D MMMM , HH:mm A')
-        }
-  }
+  methods: {
+    fromArabicToASCII(arabic) {
+      return arabic.replace(/[\u0660-\u0669\u06F0-\u06F9]/g, function (a) {
+        return String.fromCharCode((a.charCodeAt(0) & 15) + 48);
+      });
+    },
+    refreshDate() {
+      moment.locale("ar");
+      this.curDate = moment().format("dddd D MMMM , HH:mm A");
+    },
+  },
 };
 </script>
 <style lang="scss">
@@ -52,17 +53,17 @@ export default {
   width: 100px;
   height: 100px;
   position: absolute;
-  top: 347px;
+  top: 306px;
   left: 263px;
 }
 
 svg path {
-  animation: dash 2.5s linear 0s forwards infinite; 
+  animation: dash 2.5s linear 0s forwards infinite;
 }
 
 svg path {
-   stroke-dasharray: 770;
-   stroke-dashoffset: 770;
+  stroke-dasharray: 770;
+  stroke-dashoffset: 770;
 }
 
 @keyframes dash {
@@ -77,19 +78,19 @@ svg path {
 
 .sarah-font {
   font-family: myFirstFont;
-   position: absolute;
-  top: 422px;
+  position: absolute;
+  top: 381px;
   font-size: 13px;
   left: 65px;
   color: #d9ebe1e0;
 }
 .overlay {
- top: 0;
- left: 0;
- position: fixed;
- width: 100vw;
- height: 100vh;
- z-index: 99999999;
+  top: 0;
+  left: 0;
+  position: fixed;
+  width: 100vw;
+  height: 100vh;
+  z-index: 99999999;
 }
 .sarah {
   background-image: url(/sarah.png);
