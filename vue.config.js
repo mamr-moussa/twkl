b90388671860ea05e90b06a50bcb373702431073
Hesham Ahmed <36745213+heshamdev@users.noreply.github.com>
2022-01-23 02:46:34 +0200

    fix: addd sw

--- vue.config.js
+++ vue.config.js
@@ -13,6 +13,7 @@ module.exports = {
     workboxPluginMode: 'InjectManifest',
     workboxOptions: {
       // swSrc is required in InjectManifest mode.
+      swSrc: 'dev/sw.js',
       // ...other Workbox options...
     }
   }
